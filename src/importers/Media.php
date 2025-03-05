61b53b6ac4c10afd27f8216c4c058c3312526466
brandonkelly <brandon@pixelandtonic.com>
2025-03-05 10:51:07 -0800

    Don't assume WpContent has a Local FS
    Also now checks for an existing asset at the target path and updates that if found, rather than creating a duplicate asset.
    
    Fixes #14

--- src/importers/Media.php
+++ src/importers/Media.php
@@ -11,18 +11,18 @@ use Craft;
 use craft\base\ElementInterface;
 use craft\elements\Asset;
 use craft\enums\CmsEdition;
-use craft\fs\Local;
 use craft\helpers\Assets;
 use craft\helpers\DateTimeHelper;
 use craft\helpers\FileHelper;
 use craft\helpers\StringHelper;
+use craft\models\Volume;
+use craft\models\VolumeFolder;
 use craft\wpimport\BaseImporter;
 use craft\wpimport\generators\fields\Caption;
 use craft\wpimport\generators\fields\Description;
 use craft\wpimport\generators\fields\WpTitle;
-use craft\wpimport\generators\filesystems\Uploads as UploadsFs;
+use craft\wpimport\generators\filesystems\Uploads;
 use craft\wpimport\generators\volumes\WpContent;
-use craft\wpimport\generators\volumes\WpContent as UploadsVolume;
 use Throwable;
 use yii\console\Exception;
 
@@ -53,42 +53,19 @@ class Media extends BaseImporter
         return Asset::class;
     }
 
+    public function find(array $data): ?ElementInterface
+    {
+        [, $folder, $filename] = $this->fileInfo($data);
+        return Asset::find()->folderId($folder->id)->filename($filename)->one();
+    }
+
     public function populate(ElementInterface $element, array $data): void
     {
-        $wpPath = $data['media_details']['file'] ?? pathinfo($data['source_url'], PATHINFO_BASENAME);
-        $path = pathinfo($wpPath, PATHINFO_DIRNAME);
-        $filename = Assets::prepareAssetName(pathinfo($wpPath, PATHINFO_BASENAME));
-        $folder = Craft::$app->assets->ensureFolderByFullPathAndVolume($path !== '.' ? $path : '', UploadsVolume::get());
-
-        /** @var Asset $element */
-        $element->volumeId = WpContent::get()->id;
+        [$volume, $folder, $filename] = $this->fileInfo($data);
         $title = $data['title']['raw'] ?? $filename;
+        /** @var Asset $element */
         $element->title = StringHelper::safeTruncate($title, 255);
         $element->setFieldValue(WpTitle::get()->handle, $title);
-
-        /** @var Local $fs */
-        $fs = UploadsFs::get();
-        $destPath = sprintf('%s/%s/%s', $fs->getRootPath(), $path, $filename);
-
-        if (file_exists($destPath)) {
-            $element->folderId = $folder->id;
-            $element->folderPath = $folder->path;
-            $element->filename = $filename;
-            $element->kind = Assets::getFileKindByExtension($filename);
-            $element->setScenario(Asset::SCENARIO_INDEX);
-        } else {
-            $response = $this->command->client->get($data['source_url']);
-            if ($response->getStatusCode() !== 200) {
-                throw new Exception("No file found at {$data['source_url']}");
-            }
-            $tempPath = sprintf('%s/%s', Craft::$app->path->getTempAssetUploadsPath(), $filename);
-            FileHelper::writeToFile($tempPath, $response->getBody()->getContents());
-            $element->tempFilePath = $tempPath;
-            $element->newFolderId = $folder->id;
-            $element->newFilename = $filename;
-            $element->avoidFilenameConflicts = true;
-        }
-
         if (!empty($data['date_gmt'])) {
             $element->dateCreated = DateTimeHelper::toDateTime($data['date_gmt']);
         }
@@ -110,5 +87,44 @@ class Media extends BaseImporter
             Caption::get()->handle => $data['caption']['raw'] ?? null,
             Description::get()->handle => $data['description']['raw'] ?? null,
         ]));
+
+        // only update the file if it's a new asset
+        if (!$element->id) {
+            $element->volumeId = $volume->id;
+
+            $filePath = sprintf('%s%s', $folder->path ? "$folder->path/" : '', $filename);
+            if ($volume->fileExists($filePath)) {
+                $element->folderId = $folder->id;
+                $element->folderPath = $folder->path;
+                $element->filename = $filename;
+                $element->kind = Assets::getFileKindByExtension($filename);
+                $element->setScenario(Asset::SCENARIO_INDEX);
+            } else {
+                $response = $this->command->client->get($data['source_url']);
+                if ($response->getStatusCode() !== 200) {
+                    throw new Exception("No file found at {$data['source_url']}");
+                }
+                $tempPath = sprintf('%s/%s', Craft::$app->path->getTempAssetUploadsPath(), $filename);
+                FileHelper::writeToFile($tempPath, $response->getBody()->getContents());
+                $element->tempFilePath = $tempPath;
+                $element->newFolderId = $folder->id;
+                $element->newFilename = $filename;
+                $element->avoidFilenameConflicts = true;
+            }
+        }
+    }
+
+    /**
+     * @param array $data
+     * @return array{0:Volume, 1:VolumeFolder, 2:string}
+     */
+    private function fileInfo(array $data): array
+    {
+        $wpPath = $data['media_details']['file'] ?? pathinfo($data['source_url'], PATHINFO_BASENAME);
+        $path = pathinfo($wpPath, PATHINFO_DIRNAME);
+        $volume = WpContent::get();
+        $folder = Craft::$app->assets->ensureFolderByFullPathAndVolume($path !== '.' ? $path : '', $volume);
+        $filename = Assets::prepareAssetName(pathinfo($wpPath, PATHINFO_BASENAME));
+        return [$volume, $folder, $filename];
     }
 }
